c9fbcc55b8e73d23a899aef008cb2919edbe1014
Erlend Viddal <eviddal@gmail.com>
2021-03-09 17:00:26 +0100

    Added moutza keyboard

--- qmk/kyria/keymap.c
+++ qmk/kyria/keymap.c
@@ -17,89 +17,12 @@
 #include "pointing_device.h"
 #include "report.h"
 #include <stdio.h>
-
-enum layers {
-  QWERTY = 0,
-  QWERTY_MAC,
-  CMD,
-  CMD_MAC,
-  QWERTY_MOD_L,
-  QWERTY_MOD_R,
-  NAVIGATION,
-  NAVIGATION_MOD,
-  NUMBERS,
-  NUMBERS_MOD
-};
-
-enum os {
-  WINDOWS = 0,
-  MAC,
-  LINUX
-};
-
-enum custom_keycodes {
-  M_AE = SAFE_RANGE,
-  M_OE,
-  M_AA,
-  M_FIND,
-  M_FIND_CURSOR,
-  M_FIND_NEXT,
-  M_FIND_PREVIOUS,
-  M_OS_WINDOWS,
-  M_OS_LINUX,
-  M_OS_MAC,
-  M_ACCL_1,
-  M_ACCL_2,
-  M_ACCL_3,
-  M_ACCL_4,
-  M_DIRECTIVE_TEXTMOTION,
-  M_DIRECTIVE_MOUSE,
-  M_DIRECTIVE_SCROLL,
-  M_UP,
-  M_DOWN,
-  M_LEFT,
-  M_RIGHT,
-  M_MOUSE1,
-  M_TOGGLE_MOUSE1,
-  M_MOUSE2,
-  M_TOGGLE_MOUSE2,
-  M_MOUSE3,
-  M_TOGGLE_MOUSE3,
-  M_PARENTHESIS,
-  M_CURLIES,
-  M_BRACKETS
-};
-
-enum tap_dances {
-  TD_ACC1_X,
-  TD_ACC2_C,
-  TD_ACC3_V,
-};
+#include "state.c"
+#include "oled.c"
+#include "navigation.c"
 
 
-enum directive {
-  TEXTMOTION,
-  MOUSE,
-  SCROLL
-};
 
-enum DIRECTION{
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT
-};
-
-bool accelrated_1 = false;
-bool accelrated_2 = false;
-bool accelrated_3 = false;
-bool accelrated_4 = false;
-
-
-enum os active_os = MAC;
-enum directive active_directive = TEXTMOTION;
-
-static uint16_t directive_timer;
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -374,10 +297,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       M_OS_MAC, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,
       KC_ESC, KC_F7, KC_F8, KC_F9, KC_TRNS, KC_TRNS,
 
-      M_OS_LINUX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      M_OS_LINUX, M_OS_MAC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_F10, KC_F4, KC_F5, KC_F6, KC_F11, KC_BSPC,
 
-      M_OS_WINDOWS, KC_TRNS, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD, KC_TRNS, KC_TRNS,
+      M_OS_WINDOWS, M_OS_WINDOWS, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TAB, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS,
 
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -407,24 +330,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-#ifdef OLED_DRIVER_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  return OLED_ROTATION_180;
-}
-
-static void render_kyria_logo(void) {
-  static const char PROGMEM kyria_logo[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,128,192,224,240,112,120, 56, 60, 28, 30, 14, 14, 14,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7, 14, 14, 14, 30, 28, 60, 56,120,112,240,224,192,128,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,192,224,240,124, 62, 31, 15,  7,  3,  1,128,192,224,240,120, 56, 60, 28, 30, 14, 14,  7,  7,135,231,127, 31,255,255, 31,127,231,135,  7,  7, 14, 14, 30, 28, 60, 56,120,240,224,192,128,  1,  3,  7, 15, 31, 62,124,240,224,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,240,252,255, 31,  7,  1,  0,  0,192,240,252,254,255,247,243,177,176, 48, 48, 48, 48, 48, 48, 48,120,254,135,  1,  0,  0,255,255,  0,  0,  1,135,254,120, 48, 48, 48, 48, 48, 48, 48,176,177,243,247,255,254,252,240,192,  0,  0,  1,  7, 31,255,252,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,255,255,255,  0,  0,  0,  0,  0,254,255,255,  1,  1,  7, 30,120,225,129,131,131,134,134,140,140,152,152,177,183,254,248,224,255,255,224,248,254,183,177,152,152,140,140,134,134,131,131,129,225,120, 30,  7,  1,  1,255,255,254,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0,255,255,  0,  0,192,192, 48, 48,  0,  0,240,240,  0,  0,  0,  0,  0,  0,240,240,  0,  0,240,240,192,192, 48, 48, 48, 48,192,192,  0,  0, 48, 48,243,243,  0,  0,  0,  0,  0,  0, 48, 48, 48, 48, 48, 48,192,192,  0,  0,  0,  0,  0,
-    0,  0,  0,255,255,255,  0,  0,  0,  0,  0,127,255,255,128,128,224,120, 30,135,129,193,193, 97, 97, 49, 49, 25, 25,141,237,127, 31,  7,255,255,  7, 31,127,237,141, 25, 25, 49, 49, 97, 97,193,193,129,135, 30,120,224,128,128,255,255,127,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0, 63, 63,  3,  3, 12, 12, 48, 48,  0,  0,  0,  0, 51, 51, 51, 51, 51, 51, 15, 15,  0,  0, 63, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 48, 48, 63, 63, 48, 48,  0,  0, 12, 12, 51, 51, 51, 51, 51, 51, 63, 63,  0,  0,  0,  0,  0,
-    0,  0,  0,  0, 15, 63,255,248,224,128,  0,  0,  3, 15, 63,127,255,239,207,141, 13, 12, 12, 12, 12, 12, 12, 12, 30,127,225,128,  0,  0,255,255,  0,  0,128,225,127, 30, 12, 12, 12, 12, 12, 12, 12, 13,141,207,239,255,127, 63, 15,  3,  0,  0,128,224,248,255, 63, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  3,  7, 15, 62,124,248,240,224,192,128,  1,  3,  7, 15, 30, 28, 60, 56,120,112,112,224,224,225,231,254,248,255,255,248,254,231,225,224,224,112,112,120, 56, 60, 28, 30, 15,  7,  3,  1,128,192,224,240,248,124, 62, 15,  7,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  3,  7, 15, 14, 30, 28, 60, 56,120,112,112,112,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,112,112,112,120, 56, 60, 28, 30, 14, 15,  7,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-  };
-  oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
-}
 
 
 static void set_os(enum os new_os) {
@@ -434,165 +339,6 @@ static void set_os(enum os new_os) {
 }
 
 
-static void set_directive(enum directive new_directive) {
-  directive_timer = timer_read();
-  active_directive = new_directive;
-}
-
-static void pointer(int x, int y, int h, int v) {
-  report_mouse_t currentReport = pointing_device_get_report();
-  currentReport.x = x;
-  currentReport.y = y;
-  currentReport.v = v;
-  currentReport.h = h;
-  pointing_device_set_report(currentReport);
-  pointing_device_send();
-}
-
-static void mouse_down(uint8_t button) {
-  directive_timer = timer_read();
-  report_mouse_t currentReport = pointing_device_get_report();
-  currentReport.buttons |= button;
-  pointing_device_set_report(currentReport);
-  pointing_device_send();
-}
-
-static void mouse_up(uint8_t button) {
-  directive_timer = timer_read();
-  report_mouse_t currentReport = pointing_device_get_report();
-  currentReport.buttons &= ~button;
-  pointing_device_set_report(currentReport);
-  pointing_device_send();
-}
-
-static void mouse_toggle(uint8_t button) {
-  directive_timer = timer_read();
-  report_mouse_t currentReport = pointing_device_get_report();
-  currentReport.buttons ^= button;
-  pointing_device_set_report(currentReport);
-  pointing_device_send();
-}
-
-static void move(enum DIRECTION dir) {
-  directive_timer = timer_read();
-  int mult = 1;
-  int x = 0;
-  int y = 0;
-  switch (active_directive) {
-    case MOUSE:
-      mult=25;
-      if(accelrated_4)
-        mult = 1;
-      else if (accelrated_3 || accelrated_2) 
-        mult = 125;
-      else if (accelrated_1)
-        mult = 5;  
-
-      switch (dir) {
-        case UP:
-          y = 0-mult;
-          break;
-        case DOWN:
-          y = mult;
-          break;
-        case LEFT:
-          x = 0-mult;
-          break;
-        case RIGHT:
-          x = mult;
-          break;
-      }
-
-      pointer(x, y, 0, 0);
-      if(accelrated_3) {
-        pointer(x, y, 0, 0);
-        pointer(x, y, 0, 0);
-        pointer(x, y, 0, 0);
-        pointer(x, y, 0, 0);
-        pointer(1, 1, 0, 0);
-      }
-      break;
-
-    case SCROLL:
-      mult=3;
-      if(accelrated_4)
-        mult = 1;
-      else if (accelrated_3 || accelrated_2) 
-        mult = 27;
-      else if (accelrated_1)
-        mult = 9;  
-
-      switch (dir) {
-        case UP:
-          y = mult;
-          break;
-        case DOWN:
-          y = 0-mult;
-          break;
-        case LEFT:
-          x = 0-mult;
-          break;
-        case RIGHT:
-          x = mult;
-          break;
-      }
-      if (active_os==MAC)
-        y = 0-y;
-
-      pointer(0, 0, x, y);
-      if (active_os==MAC)
-        pointer(0, 0, x, y);
-      if(accelrated_3) {
-        pointer(0, 0, x, y);
-        pointer(0, 0, x, y);
-        pointer(0, 0, x, y);
-        pointer(0, 0, x, y);
-      }
-      break;
-
-    case TEXTMOTION:
-      switch (dir) {
-        case UP:
-          register_code(KC_UP);
-          break;
-        case DOWN:
-          register_code(KC_DOWN);
-          break;
-        case LEFT:
-          register_code(KC_LEFT);
-          break;
-        case RIGHT:
-          register_code(KC_RIGHT);
-          break;
-      }
-      break;
-
-  }
-}
-
-static void release_move(enum DIRECTION dir) {
-  switch (active_directive) {
-    case TEXTMOTION:
-      switch (dir) {
-        case UP:
-          unregister_code(KC_UP);
-          break;
-        case DOWN:
-          unregister_code(KC_DOWN);
-          break;
-        case LEFT:
-          unregister_code(KC_LEFT);
-          break;
-        case RIGHT:
-          unregister_code(KC_RIGHT);
-          break;
-      }
-      break;
-    case SCROLL:
-    case MOUSE:
-      break;
-  }
-}
 
 void shifted_tap(uint16_t key, uint16_t key_on_shift) {
     if(get_mods() & MOD_BIT(KC_LSHIFT))
@@ -611,14 +357,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
       case M_AE:
         if(active_os==MAC) SEND_STRING(SS_LALT("'"));
-        if(active_os==WINDOWS) SEND_STRING(SS_LALT("'"));
-        if(active_os==LINUX) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00f8" SS_TAP(X_ENT));
+        else if(active_os==WINDOWS && shifted) {
+          if(lshift) unregister_code(KC_LSHIFT);
+          if(rshift) unregister_code(KC_RSHIFT);
+          SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_9) SS_TAP(X_KP_8)));
+        }
+        else if(active_os==WINDOWS)
+          SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_0)));
+        else if(active_os==LINUX && shifted) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00c6" SS_TAP(X_ENT));
+        else if(active_os==LINUX) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00e6" SS_TAP(X_ENT));
         break;
       case M_OE:
-        SEND_STRING(SS_LALT("o"));
+        if(active_os==MAC) SEND_STRING(SS_LALT("o"));
+        else if(active_os==WINDOWS && shifted) {
+          if(lshift) unregister_code(KC_LSHIFT);
+          if(rshift) unregister_code(KC_RSHIFT);
+          SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_1) SS_TAP(X_KP_6)));
+        }
+        else if(active_os==WINDOWS)
+          SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_TAP(X_KP_8)));
+        else if(active_os==LINUX && shifted) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00d8" SS_TAP(X_ENT));
+        else if(active_os==LINUX) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00f8" SS_TAP(X_ENT));
         break;
       case M_AA:
-        SEND_STRING(SS_LALT("a"));
+        if(active_os==MAC) SEND_STRING(SS_LALT("a"));
+        else if(active_os==WINDOWS && shifted) {
+          if(lshift) unregister_code(KC_LSHIFT);
+          if(rshift) unregister_code(KC_RSHIFT);
+          SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_9) SS_TAP(X_KP_7)));
+        }
+        else if(active_os==WINDOWS)
+          SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_9)));
+        else if(active_os==LINUX && shifted) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00c5" SS_TAP(X_ENT));
+        else if(active_os==LINUX) SEND_STRING(SS_LCTL(SS_LSFT("u")) "00e5" SS_TAP(X_ENT));
         break;
       case M_ACCL_1:
         accelrated_1 = true; 
@@ -712,10 +483,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           if(lshift) unregister_code(KC_LSHIFT);
           if(rshift) unregister_code(KC_RSHIFT);
           tap_code(KC_RBRACKET);
-          if(lshift) register_code(KC_LSHIFT);
-          if(rshift) register_code(KC_RSHIFT);
         }
     }
+    if(lshift) register_code(KC_LSHIFT);
+    if(rshift) register_code(KC_RSHIFT);
   } else {
     switch (keycode) {
       case M_ACCL_1:
@@ -756,102 +527,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 };
 
-static void render_qmk_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
-}
-
-
-static void render_status(void) {
-  // QMK Logo and version information
-  render_qmk_logo();
-  oled_write_P(PSTR("       Kyria rev1.0\n\n"), false);
-
-  if(active_directive != TEXTMOTION && timer_elapsed(directive_timer) > 15000) {
-    active_directive = TEXTMOTION;
-  }
-  // Host Keyboard Layer Status
-  oled_write_P(PSTR("Layer: "), false);
-  switch (get_highest_layer(layer_state)) {
-    case QWERTY:
-      oled_write_P(PSTR("Qwerty\n"), false);
-      break;
-    case QWERTY_MAC:
-      oled_write_P(PSTR("Qwerty (M)\n"), false);
-      break;
-    case QWERTY_MOD_L: case QWERTY_MOD_R:
-      oled_write_P(PSTR("Mod(Qerty)\n"), false);
-      break;
-    case NAVIGATION:
-      oled_write_P(PSTR("Navigation\n"), false);
-      break;
-    case NAVIGATION_MOD:
-      oled_write_P(PSTR("Mod(Nav)\n"), false);
-      break;
-    case NUMBERS:
-      oled_write_P(PSTR("Numeric\n"), false);
-      break;
-    case NUMBERS_MOD:
-      oled_write_P(PSTR("Mod(Numeric)\n"), false);
-      break;
-    default:
-      oled_write_P(PSTR("Undefined\n"), false);
-  }
-
-  switch (active_os) {
-    case MAC:
-      oled_write_P(PSTR("MACINTOSH     "), false);
-      break;
-    case WINDOWS:
-      oled_write_P(PSTR("WINDOWS       "), false);
-      break;
-    case LINUX:
-      oled_write_P(PSTR("LUNIX         "), false);
-      break;
-    default:
-      oled_write_P(PSTR("Undefined     "), false);
-  }
-  
-  (active_directive) == TEXTMOTION ? oled_write_P(PSTR("ARROWS "), false)
-  : (active_directive) == MOUSE ? oled_write_P(PSTR("MOUSE  "), false)
-  :(active_directive) == SCROLL ? oled_write_P(PSTR("SCROLL "), false)
-    :oled_write_P(PSTR("UNKNOWN"), false);
-
-
-  // Host Keyboard LED Status
-  //uint8_t led_usb_state = host_keyboard_leds();
-  //oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK)    ? PSTR("NUMLCK") : PSTR("      "), false);
-  //oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK)   ? PSTR("CAPLCK") : PSTR("      "), false);
-  //oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK") : PSTR("      "), false);
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  //if(get_highest_layer(state) < QWERTY_MOD_L && IS_LAYER_ON_STATE(state, CMD)) {
-  if(get_highest_layer(state) < QWERTY_MOD_L) 
-    return state & (~((1 << CMD) | (1 << CMD_MAC)));
-  
-  if(active_os == MAC)
-    return state  | (1 << CMD_MAC) | (1 << CMD);
-  return state  | (1 << CMD);
-}
-
-void suspend_power_down_user(void) {
-  if(active_os == MAC)
-    layer_on(QWERTY_MAC);
-}
-
-void oled_task_user(void) {
-  if (is_keyboard_master()) {
-    render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
-  } else {
-    render_kyria_logo();
-  }
-}
-#endif
 
 #ifdef ENCODER_ENABLE
 void encoder_update_user(uint8_t index, bool antiClockwise) {
